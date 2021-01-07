79fb1cb6c594a3184ddfb0c30ec0b9dcd2d741f0
Gideon Weissman <gideon.weissman@gmail.com>
2021-01-06 23:56:51 -0500

    First repo

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+<title>Get CFPB complaint data</title>
+
+<style>
+table, tr, td, th {
+  border: 1px solid black;
+  border-collapse: collapse;
+  text-align: right;
+  padding: 3px;
+}
+
+table {
+  max-width: 300px;
+}
+
+</style>
+</head>
+
+<body>
+
+<form>
+  <label for="trend_interval">Trend interval:</label>
+  <select id="trend_interval" name="trend_interval">
+  </select>  <br><br>
+  
+  <label for="lens">Lens:</label>
+  <select id="lens" name="lens">
+  </select>  <br><br>
+  <label for="date_received_min">Date min:</label>
+  <input type="date" id="date_received_min" name="date_received_min"><br><br>
+  <label for="date_received_max">Date max:</label>
+  <input type="date" id="date_received_max" name="date_received_max"><br><br>
+  
+  <input type="submit" value="Submit">
+</form> 
+<br><br>
+<a id="reset" href="">Reset</a>
+
+<br><br>
+<strong>Results</strong>
+
+<br><br>
+<div>
+Total complaints: <span id="total_complaints"></span>
+
+<br><br>
+
+Complaints by date:<br>
+<a id="dl_complaints_by_date"></a>
+<table id="complaints_by_date" >
+<tr>
+<th>Period beginning</th><th># complaints</th>
+</tr>
+</table>
+
+<br><br>
+Complaints by product:<br>
+<a id="dl_complaints_by_product"></a>
+<table id="complaints_by_product" >
+<tr>
+<th>Product name</th><th># complaints</th>
+</tr>
+</table>
+</div>
+
+<?php
+// query the CFPB API
+if (!empty($_GET)) {
+    $url = "https://www.consumerfinance.gov/data-research/consumer-complaints/search/api/v1/trends?";
+    $response = file_get_contents($url . http_build_query($_GET));
+} else {
+    $response = 0;
+};
+
+?>
+
+<script>
+//load data
+const data = <?php echo $response?>;
+
+// set up dropdowns
+const dropdowns = {'trend_interval': ['year', 'quarter', 'month', 'week', 'day'], 'lens': ['overview', 'product']}
+
+for (dropdown in dropdowns){
+    for (option of dropdowns[dropdown]){
+      let option_el = document.createElement('option');
+      option_el.value = option;
+      option_el.innerText = option;
+      document.getElementById(dropdown).appendChild(option_el);
+    };
+};
+
+</script>
+
+<script type="text/javascript" src="src/cfpb_script.js"></script>
+
+</body>
+</html> 
---
+++ src/cfpb_script.js
@@ -0,0 +1,100 @@
+// SCRIPT FOR PROCESSING CFPB DATA
+
+// Set reset button
+document.getElementById('reset').href = window.location.pathname;
+
+// Make all forms match whatever the query is, to retain state.
+function replaceForms(value, key){
+    const el = document.getElementById(key);
+    el.value = value;
+};
+
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+
+if (queryString){
+    params.forEach((value, key) => replaceForms(value, key))
+} else {
+  // set default max date to today, and default min date to last year.
+  dateMax = new Date();
+  dateMin = new Date();
+  dateMin.setFullYear(dateMin.getFullYear()-2);
+  document.getElementById('date_received_min').value = dateMin.toLocaleDateString('en-CA');
+  document.getElementById('date_received_max').value = dateMax.toLocaleDateString('en-CA');
+};
+
+// create CSV strings for exporting
+let dateCSV = 'data:text/csv;charset=utf-8,period beginning,complaints';
+let productCSV = 'data:text/csv;charset=utf-8,product,complaints';
+
+
+if (data) {
+    // Load total # of complaints to page
+    total_complaints = data['aggregations']['dateRangeArea']['doc_count'];
+    document.getElementById('total_complaints').innerHTML = total_complaints.toLocaleString();
+
+    function standardDate(date){
+      let offset = date.getTimezoneOffset();
+      date.setMinutes(date.getMinutes() + offset);
+      return date.toLocaleDateString()
+    };
+
+
+
+    // create the complaints by date table.
+    const byDate = data['aggregations']["dateRangeArea"]['dateRangeArea']["buckets"];
+    
+
+    for (let dateObj of byDate){
+        let row = document.createElement('tr');
+        
+        let rawDate = new Date(dateObj['key']);
+        let date = standardDate(rawDate);
+        
+        let dateCol = document.createElement('td');
+        dateCol.innerText = date;
+        row.appendChild(dateCol);
+
+        let complaints = dateObj['doc_count'].toLocaleString();
+
+        let compCol = document.createElement('td');
+        compCol.innerText = complaints;
+        row.appendChild(compCol);
+
+        table = document.getElementById("complaints_by_date");
+        table.appendChild(row);
+
+        // make CSV
+        dateCSV += '\n' + date + ',' + String(dateObj['doc_count'])
+    };
+
+    // create complaints by product table
+    const byProduct = data['aggregations']['product']['product']['buckets'];
+
+    for (let prodObj of byProduct){
+        let row = document.createElement('tr');
+        
+        let product = prodObj['key'];
+                
+        let prodCol = document.createElement('td');
+        prodCol.innerText = product;
+        row.appendChild(prodCol);
+
+        let complaints = prodObj['doc_count'].toLocaleString();
+
+        let compCol = document.createElement('td');
+        compCol.innerText = complaints;
+        row.appendChild(compCol);
+
+        table = document.getElementById("complaints_by_product");
+        table.appendChild(row);
+    };
+    
+    // create CSVs
+    const encodedDateCSV = encodeURI(dateCSV);
+    const date_dl_el = document.getElementById('dl_complaints_by_date');
+    date_dl_el.setAttribute('href', encodedDateCSV);
+    date_dl_el.setAttribute('download', 'complaints_by_date.csv');
+    date_dl_el.innerText = '(Download data)'
+
+};
